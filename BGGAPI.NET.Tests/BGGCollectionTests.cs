ebaa45d16458c34d9b2f31bd9af2ad6f5883eaf1
Phil Kendall <phil.kendall@controlf1.co.uk>
2015-08-25 22:23:45 +0100

    First BGGCollection.Item test.

--- BGGAPI.NET.Tests/BGGCollectionTests.cs
+++ BGGAPI.NET.Tests/BGGCollectionTests.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using System;
+using BGGAPI.BGGCollectionObjects;
+using NUnit.Framework;
 
 namespace BGGAPI.NET.Tests
 {
@@ -45,6 +47,58 @@ namespace BGGAPI.NET.Tests
 
             Assert.IsNull(ranking.Position);
             Assert.IsNull(ranking.BayesianAverageRating);
+        }
+
+        [Test]
+        public void TestItemConversion()
+        {
+            var rawItem = new Item
+            {
+                ObjectType = "thing",
+                ObjectId = 1,
+                Subtype = "boardgame",
+                CollId = 2,
+                Name = "My Funky Boardgame",
+                YearPublished = "2015",
+                Image = "//localhost",
+                Thumbnail = "//localhost",
+                NumPlays = 3,
+
+                Status = new Status
+                {
+                    Own = 1,
+                    PrevOwned = 0,
+                    ForTrade = 1,
+                    Want = 0,
+                    WantToPlay = 1,
+                    WantToBuy = 0,
+                    Wishlist = 1,
+                    Preordered = 0,
+                    LastModified = new DateTime(2015, 8, 25, 21, 14, 32, DateTimeKind.Utc)
+                }
+            };
+
+            var item = new BGGCollection.Item(rawItem);
+
+            Assert.AreEqual("thing", item.Type);
+            Assert.AreEqual(1, item.Id);
+            Assert.AreEqual("boardgame", item.Subtype);
+            Assert.AreEqual(2, item.CollectionId);
+            Assert.AreEqual("My Funky Boardgame", item.Name);
+            Assert.AreEqual("2015", item.YearPublished);
+            Assert.AreEqual("//localhost", item.Image);
+            Assert.AreEqual("//localhost", item.Thumbnail);
+            Assert.AreEqual(3, item.NumberOfPlays);
+
+            Assert.IsTrue(item.Owned);
+            Assert.IsFalse(item.PreviouslyOwned);
+            Assert.IsTrue(item.AvailableForTrade);
+            Assert.IsFalse(item.WantInTrade);
+            Assert.IsTrue(item.WantToPlay);
+            Assert.IsFalse(item.WantToBuy);
+            Assert.IsTrue(item.OnWishlist);
+            Assert.IsFalse(item.Preordered);
+            Assert.AreEqual(new DateTime(2015, 8, 25, 21, 14, 32, DateTimeKind.Utc), item.StatusLastModified);
         }
     }
 }
