3370ba3011ea13b835267ed3e58f9a0ee078147f
Phil Kendall <phil.kendall@controlf1.co.uk>
2015-08-25 22:03:04 +0100

    Add constraint.

--- BGGAPI.NET/AutomapperExtensions.cs
+++ BGGAPI.NET/AutomapperExtensions.cs
@@ -5,7 +5,9 @@ namespace BGGAPI
 {
     public static class AutoMapperExtensions
     {
-        public static void NullSafeConvertUsing<TSource, TDestination>(this IMappingExpression<TSource, TDestination> mappingExpression, Func<TSource, TDestination> mappingFunction)
+        public static void NullSafeConvertUsing<TSource, TDestination>(
+            this IMappingExpression<TSource, TDestination> mappingExpression,
+            Func<TSource, TDestination> mappingFunction) where TSource : class
         {
             mappingExpression.ConvertUsing(src => src == null ? default(TDestination) : mappingFunction(src));
         }
